86a11673d6d0329b3626706174b6396c1b6133bd
Mehdi Gassem <mehdi.gassem@esprit.tn>
2021-11-16 22:00:24 +0100

    first commit

---
+++ src/Entity/Facture.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FactureRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FactureRepository::class)
+ */
+class Facture
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_facture;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Reservation::class, mappedBy="facture", orphanRemoval=true)
+     */
+    private $reservation;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Ref;
+
+    public function __construct()
+    {
+        $this->reservation = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateFacture(): ?\DateTimeInterface
+    {
+        return $this->date_facture;
+    }
+
+    public function setDateFacture(\DateTimeInterface $date_facture): self
+    {
+        $this->date_facture = $date_facture;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Reservation[]
+     */
+    public function getReservation(): Collection
+    {
+        return $this->reservation;
+    }
+
+    public function addReservation(Reservation $reservation): self
+    {
+        if (!$this->reservation->contains($reservation)) {
+            $this->reservation[] = $reservation;
+            $reservation->setFacture($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReservation(Reservation $reservation): self
+    {
+        if ($this->reservation->removeElement($reservation)) {
+            // set the owning side to null (unless already changed)
+            if ($reservation->getFacture() === $this) {
+                $reservation->setFacture(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getRef(): ?string
+    {
+        return $this->Ref;
+    }
+
+    public function setRef(string $Ref): self
+    {
+        $this->Ref = $Ref;
+
+        return $this;
+    }
+    public function __toString() {
+        return $this->Ref;
+    }
+
+}
---
+++ src/Entity/Reservation.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+/**
+ * @ORM\Entity(repositoryClass=ReservationRepository::class)
+ */
+class Reservation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Assert\DateTime
+     * @var string A "d-m-Y  H:i:s" formatted value
+     */
+    private $date_reservation;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Facture::class, inversedBy="reservation")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $facture;
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateReservation(): ?\DateTimeInterface
+    {
+        return $this->date_reservation;
+    }
+
+    public function setDateReservation(\DateTimeInterface $date_reservation): self
+    {
+        $this->date_reservation = $date_reservation;
+
+        return $this;
+    }
+
+    public function getFacture(): ?Facture
+    {
+        return $this->facture;
+    }
+
+    public function setFacture(?Facture $facture): self
+    {
+        $this->facture = $facture;
+
+        return $this;
+    }
+    public function __toString() {
+        return $this->id;
+    }
+
+}
